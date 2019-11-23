184707328d8c14340177273af749767b630f6039
MarinaYsn <marinaysn@gmail.com>
2019-11-23 08:46:42 -0500

    created feeds router and controller

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+const feedRoutes = require('./routes/feed')
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+app.use('/feed', feedRoutes);
 
 app.listen(8080);
---
+++ controllers/feed.js
@@ -0,0 +1,16 @@
+
+exports.getPosts = ((req, res, next)=>{
+
+    res.status(200).json({posts: [{title: 'Creating REST APIs with Node.js & TypeScript', content: 'A WebAPI consisting of endpoints to a request–response message system (JSON/XML) exposed as an HTTP-based server', url: 'https://www.reddit.com/r/learnjavascript/comments/dzqbay/creating_rest_apis_with_nodejs_typescript_part_1/'}]});
+});
+
+exports.createPost = ( req, res, next) => {
+    //Create post in db
+    const title = req.body.title;
+    const content = req.body.content;
+    res.status(201).json({
+        message: 'Post Created!',
+        post: {id: new Date().toISOString(), title: title, content: content}
+    })
+};
+
