8fc6d749b5e49ed19a1c32292c298d2ebadd0329
MarinaYsn <marinaysn@gmail.com>
2019-11-30 16:15:35 -0500

    added helping routes for status call updated

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -25,7 +25,12 @@ class Feed extends Component {
   };
 
   componentDidMount() {
-    fetch('URL')
+    fetch('http://localhost:8080/feed/status',
+    {method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + this.props.token
+    }})
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch user status.');
@@ -82,9 +87,18 @@ class Feed extends Component {
   };
 
   statusUpdateHandler = event => {
+
     event.preventDefault();
-    fetch('URL')
+    fetch('http://localhost:8080/feed/status/',
+    {method: 'POST',
+    body: JSON.stringify({status: this.state.status}),
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + this.props.token
+    }
+  })
       .then(res => {
+
         if (res.status !== 200 && res.status !== 201) {
           throw new Error("Can't update status!");
         }
