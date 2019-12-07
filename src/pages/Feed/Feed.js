58fb671285d8c44d8a222d9ee15ad1e7e3792aae
MarinaYsn <marinaysn@gmail.com>
2019-12-06 23:04:23 -0500

    updated create and update post pages

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -96,6 +96,8 @@ class Feed extends Component {
     }
     let page = this.state.postPage;
 
+    console.log(page);
+
     if (direction === 'next') {
 
       page++;
@@ -105,26 +107,55 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('http://localhost:8080/feed/posts?page=' + page, {
-      headers: {
-        Authorization: 'Bearer ' + this.props.token
+
+    const graphqlQuery = {
+      query: `{
+      posts(page: ${page}){
+        posts {
+          _id
+          title
+          content
+          creator{
+            name
+          }
+          createdAt
+        }
+        totalPosts
       }
+    }
+      `
+    }
+
+    fetch('http://localhost:8080/graphql', {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + this.props.token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(graphqlQuery)
     })
       .then(res => {
-        if (res.status !== 200) {
-          throw new Error('Failed to fetch posts.');
-        }
+
         return res.json();
       })
       .then(resData => {
+
+        console.log(resData);
+
+        if (resData.errors) {
+          throw new Error(
+            "Fail to fetch posts!"
+          );
+        }
+
         this.setState({
-          posts: resData.posts.map(post => {
+          posts: resData.data.posts.posts.map(post => {
             return {
               ...post,
               imagePath: post.imageUrl
             }
           }),
-          totalPosts: resData.totalItems,
+          totalPosts: resData.data.posts.totalPosts,
           postsLoading: false
         });
       })
@@ -186,43 +217,80 @@ class Feed extends Component {
     formData.append('content', postData.content);
     formData.append('image', postData.image);
 
-    let url = 'http://localhost:8080/feed/post';
-    let method = 'POST';
-    if (this.state.editPost) {
-      url = 'http://localhost:8080/feed/post/' + this.state.editPost._id;
-      method = 'PUT'
+    let graphqlQuery = {
+      query: `
+      mutation{
+        createPost(
+          postInput: {title: "${postData.title}", 
+          content: "${postData.content}", 
+            imageUrl: "www.hereIsTheTestUrl.com/1.jpeg"}){
+          _id
+          title
+          content
+          imageUrl
+          creator {
+            name
+          }
+          createdAt
+          updatedAt
+        }
+      }`
     }
 
 
-    fetch(url, {
-      method: method,
-      body: formData,
-      headers: {
-        Authorization: 'Bearer ' + this.props.token
-      }
-    })
-      .then(res => {
+    fetch('http://localhost:8080/graphql',
+      {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer ' + this.props.token,
+          'Content-Type': 'application/json'
 
-        console.log(res.status);
-        if (res.status !== 200 && res.status !== 201) {
-          throw new Error('Creating or editing a post failed!');
-        }
+        },
+        body: JSON.stringify(graphqlQuery)
+      })
+      .then(res => {
         return res.json();
       })
       .then(resData => {
+        if (resData.errors && (resData.errors[0].code === 422 || resData.errors[0].code === 500)) {
+          throw new Error(
+            resData.errors[0].message
+          );
+        }
+
+        if (resData.errors) {
+          throw new Error(
+            "Cannot create user!"
+          );
+        }
+
+        console.log(resData);
         const post = {
-          _id: resData.post._id,
-          title: resData.post.title,
-          content: resData.post.content,
-          creator: resData.post.creator,
-          createdAt: resData.post.createdAt
+          _id: resData.data.createPost._id,
+          title: resData.data.createPost.title,
+          content: resData.data.createPost.content,
+          creator: resData.data.createPost.creator,
+          createdAt: resData.data.createPost.createdAt
         };
-        this.setState(prevState => {
-          return {
-            isEditing: false,
-            editPost: null,
-            editLoading: false
-          };
+       
+          this.setState(prevState => {
+            let updatedPosts = [...prevState.posts];
+            if (prevState.editPost) {
+              const postIndex = prevState.posts.findIndex(
+                p => p._id === prevState.editPost._id
+              );
+              updatedPosts[postIndex] = post;
+            } else {
+              updatedPosts.pop();
+              updatedPosts.unshift(post);
+            }
+            return {
+              posts: updatedPosts,
+              isEditing: false,
+              editPost: null,
+              editLoading: false
+            };
+          
         });
       })
       .catch(err => {
